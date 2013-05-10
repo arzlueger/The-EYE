12990b99d04b6475f68a2605e9e8bc0682da12c5
André Lueger <andre.lueger@gmail.com>
2013-05-10 23:41:24 +0200

    Updated Version

---
+++ Help_German.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <iostream>
+#include <stdlib.h>
+void Help_German()
+{
+}
--- Main_menu_German.cpp
+++ Main_menu_German.cpp
@@ -3,23 +3,28 @@
 #include <conio.h>
 #include <iostream>
 #include <stdlib.h>
-int Difficulty_German()
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <iostream>
+#include <stdlib.h>
+int Main_menu_German()
 {
-	int Difficulty=2;//Speaks for itself. It is the indicator for the difficulty in the Game.//
+	int Menu=1;//Speaks for itself. It is the indicator for the Menu in the Game.//
 	char HI;//HI means Human Inteface and that is exactly what it does.//
 	while(1==1)
 	{
-		printf("Benutze WASD zum steuern\nBenutze E als OK\n\nDifficulty:\n\"Homer Simpson\"\n\"\Forrest Gump\"\n\"Stephen Hawking\"\n\n");//More are coming in later releases.//
-		switch(Difficulty)//Used for the grahical feedback to the user.//
+		printf("Use WASD to control\nUse E for OK\n\nMenu:\n\"Start new game\"\n\"\Help\"\n\"Exit\"\n\n");//More are coming in later releases.//
+		switch(Menu)//Used for the grahical feedback to the user.//
 		{
 			case 1:
-				printf("\"Homer Simpson\"");//Easy//
+				printf("\"Start new game\"");//Easy//
 				break;
 			case 2:
-				printf("\"Forrest Gump\"");//Medium//
+				printf("\"Help\"");//Medium//
 				break;
 			case 3:
-				printf("\"Stephen Hawking\"");//Hard//
+				printf("\"Exit\"");//Hard//
 				break;
 		}
 		HI=getch();
@@ -27,16 +32,16 @@ int Difficulty_German()
 		{
 			break;
 		}
-		if(HI=='w'&&Difficulty>1)//Up//
+		if(HI=='w'&&Menu>1)//Up//
 		{
-			Difficulty--;
+			Menu--;
 		}
-		if(HI=='s'&&Difficulty<3)//Down//
+		if(HI=='s'&&Menu<3)//Down//
 		{
-			Difficulty++;
+			Menu++;
 		}
 		system("cls");//The magic and overhelming "Clear Screen".//
 	}
 	system("cls");//A blank page for a new menu.//
-	return Difficulty;
+	return Menu;
 }
